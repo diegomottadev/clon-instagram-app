54c2af6797e8f14913f0ab08cde0a9fbd2344804
diego <diegomotta18@gmail.com>
2020-12-18 13:56:21 -0300

    Implementar Markup de Vista Post.js

--- src/Vistas/Post.js
+++ src/Vistas/Post.js
@@ -8,33 +8,33 @@ import { Link } from 'react-router-dom';
 import Axios from 'axios';
 import RecursoNoExiste from '../Componetes/RecursoNoExiste';
 //match : prop de route router para acceder al id por parametros
-export default function Post({ mostrarError, match }) {
+export default function PostVista({ mostrarError, match }) {
     const postId = match.params.id;
-    const [post,setPost] = useState(null);
-    const [loading, setLoading ] = useState(true);
+    const [post, setPost] = useState(null);
+    const [loading, setLoading] = useState(true);
     const [postNoExiste, setPostNoExiste] = useState(false);
     // cuando ingresa a esta vista el lloading se carga primero y 
     // mientras el servidor buscar el post
-    useEffect(()=>{
+    useEffect(() => {
 
-        async function cargarPost(){
+        async function cargarPost() {
             try {
-                const {data: post} = await Axios.get(`/api/posts/${postId}`);
+                const { data: post } = await Axios.get(`/api/posts/${postId}`);
                 setPost(post);
                 setLoading(false);
             } catch (error) {
-                if(error.response & error.response.status == 404 || error.response.status == 400){
+                if (error.response & error.response.status == 404 || error.response.status == 400) {
                     setPostNoExiste(true);
-                }else{
+                } else {
                     mostrarError('Hubo un problema cargar este post');
                 }
                 setLoading(false);
             }
         }
         cargarPost();
-    },[postId]); //en caso que la postId cambie el useEffect se de cuenta y cambie el post
+    }, [postId]); //en caso que la postId cambie el useEffect se de cuenta y cambie el post
 
-    if(loading){
+    if (loading) {
         return (
             <Main center>
                 <Loading></Loading>
@@ -42,18 +42,69 @@ export default function Post({ mostrarError, match }) {
         );
     }
 
-    if(postNoExiste){
-        return <RecursoNoExiste mensaje='El post que estas intentando ver no existe'/>;
+    if (postNoExiste) {
+        return <RecursoNoExiste mensaje='El post que estas intentando ver no existe' />;
     }
 
-    if(post == null){
+    if (post == null) {
         return null;
     }
 
     return (
         <Main center>
-            <h1> Soy el post</h1>
+            <Post  {...post} />
         </Main>
     );
 
+}
+
+function Post({
+    comentarios,
+    caption,
+    url,
+    usuario,
+    estaLike,
+    onSubmitLike,
+    onSubmitComentario
+}) {
+    return (
+        <div className="Post">
+            <div className="Post__image-container">
+                <img src={url} alt={caption} />
+            </div>
+            <div className="Post__side-bar">
+                <Avatar usuario={usuario}></Avatar>
+                <div className="Post__comentario-y-like">
+                    <Comentarios usuario={usuario} caption={caption} comentarios={comentarios}></Comentarios>
+                    <div className="Post__like">
+                        <BotonLike onSubmitLike={onSubmitLike} like={estaLike}></BotonLike>
+                    </div>
+                </div>
+                <Comentar onSubmitComentario={onSubmitComentario}></Comentar>
+            </div>
+        </div>
+    )
+}
+
+function Comentarios({ usuario, caption, comentarios }) {
+    return (
+        <ul className="Post__comentarios">
+            <li className="Post__comentario">
+                <Link to={`/perfil/${usuario.username}`} className="Post__autor-comentario">
+                    <b>{usuario.username}</b>
+                </Link>
+                {''}{caption}
+            </li>
+            {
+                comentarios.map(comentario => (
+                    <li key={comentario._id} className="Post__comentario">
+                        <Link to={`/perfil/${comentario.usuario.username}`} className="Post__autor-comentario">
+                            <b>{comentario.usuario.username}</b>{''}
+                        </Link>
+                        {' '}{comentario.mensaje}
+                    </li>
+                ))
+            }
+        </ul>
+    )
 }
